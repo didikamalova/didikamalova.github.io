390b7ecccfca04f7bcbc73562bf775f0391037ba
Dilnaz <dilnaz@stanford.edu>
2024-08-11 21:13:53 -0700

    exp

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,6 +54,18 @@
     this.classList.toggle('bi-x')
   })
 
+   /**
+   * Toggle mobile nav dropdowns
+   */
+  document.querySelectorAll('.navmenu .toggle-dropdown').forEach(navmenu => {
+    navmenu.addEventListener('click', function(e) {
+      e.preventDefault();
+      this.parentNode.classList.toggle('active');
+      this.parentNode.nextElementSibling.classList.toggle('dropdown-active');
+      e.stopImmediatePropagation();
+    });
+  });
+
   /**
    * Scrool with ofset on links with a class name .scrollto
    */
